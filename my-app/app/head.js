11d41085b4a083732f3c8a9e65176c534c99199f
ridvan-byr <ridvanemrebayar@gmail.com>
2025-07-09 17:43:23 +0300

    Add Google verification meta tag to head

---
+++ my-app/app/head.js
@@ -0,0 +1,7 @@
+export default function Head() {
+    return (
+      <>
+        <meta name="google-site-verification" content="krPvtUAv6kOWL-frVHXvyelrx7f17zVjJniKp_9BdwQ" />
+      </>
+    );
+  }
